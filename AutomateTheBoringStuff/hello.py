173a87058839bdc12d08f61a527399609f2c3a57
Alicia Vigil <90734184+ajvigil@users.noreply.github.com>
2022-05-16 17:10:15 -0600

    for/while loops

--- AutomateTheBoringStuff/hello.py
+++ AutomateTheBoringStuff/hello.py
@@ -38,3 +38,43 @@ elif age > 2000:
     print('Unlike you, Alice is not an undead, imortal vampire.')
 elif age > 100:
     print('You are not Alice, grannie.')
+
+# while loops
+
+spam = 0 
+while spam < 5:
+    print('Hello World!')
+    spam = spam + 1
+
+# example 2
+
+name = ''
+while name != 'your name':
+    print('Please type your name.')
+    name = input()
+    if name == 'your name':
+        break
+print('Thank you!')
+
+# example 3, continue statements 
+
+spam = 0 
+while spam < 5:
+    spam = spam + 1
+    if spam == 3:
+        continue
+    print('spam is ' + str(spam))
+
+# for loops 
+
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times ' + str(i))
+
+# example 2
+
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+
