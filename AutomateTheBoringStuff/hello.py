85c0acad496214dcfaed7fcda99fcd28e5ec7115
Alicia Vigil <90734184+ajvigil@users.noreply.github.com>
2022-05-16 16:53:17 -0600

    if else elif

--- AutomateTheBoringStuff/hello.py
+++ AutomateTheBoringStuff/hello.py
@@ -9,3 +9,32 @@ print(len(myName))
 print('What is your age?')
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
+
+## If, Else and Elif Statements 
+
+name = 'James'
+if name == 'James':
+    print('Hi James')
+print('Done')
+
+# another example
+
+password = 'swordfish'
+if password == 'swordfish':
+    print('Access Granted')
+else:
+    print('Wrong Password')
+
+# another 
+
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12: 
+    print('You are not ALice, kiddo.')
+elif age > 2000: 
+    print('Unlike you, Alice is not an undead, imortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
